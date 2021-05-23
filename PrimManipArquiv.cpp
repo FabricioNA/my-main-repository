6e20cb67f6515aea3a0c99570ec0519f1b88585e
FabricioNA <fabricioaleixo@aluno.multivix.edu.br>
2021-05-23 17:51:21 -0300

    nome com ponteiro e malloc

--- PrimManipArquiv.cpp
+++ PrimManipArquiv.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
+#include <cstdio>
+#include <cstdlib>
 
 using namespace std;
 
 void function_teste(string teste);
 
 int main(int argc, const char** argv) {
+    char *name = (char *) malloc(sizeof(char)+1);
+     
     return EXIT_SUCCESS;
 }
