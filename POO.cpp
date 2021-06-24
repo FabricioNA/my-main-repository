1d6db0e4d80833f8addb9086eeee4af31543a75a
FabricioNA <fabricioaleixo@aluno.multivix.edu.br>
2021-06-24 18:56:28 -0300

    .

---
+++ POO.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+struct endereco {
+	string logr;
+	string bairro;
+	string cidade;
+	char uf[2];
+};
+
+class cliente{
+
+public:
+    string nome;
+    int idade;
+    char sexo;
+    string faixa_etaria;
+    void selec(int tp);
+
+private:
+    endereco end;
+
+};
+// o -> substitui o . para se referir como acontece na struct
+void cliente::selec(int tp){
+    switch (tp){
+    case 1:
+        this -> faixa_etaria = "Crianca";
+        break;
+    case 2:
+        this -> faixa_etaria = "Adulto";
+        break;
+    case 3:
+        this -> faixa_etaria = "Idoso";
+        break;
+    default:
+        break;
+    }
+}
+
+int main(int argc, const char** argv) {
+    cliente *cl1 = new cliente();
+    cl1 ->selec(1);
+    cout << cl1 -> faixa_etaria;
+
+    return EXIT_SUCCESS;
+}
